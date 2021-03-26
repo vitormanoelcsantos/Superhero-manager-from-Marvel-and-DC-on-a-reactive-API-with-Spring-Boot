5be0e8644b0ceb44b0306b39f63fa01d5d43c90f
vitormanoelcsantos <vitormanoelcsantos@outlook.com>
2021-03-26 17:18:38 -0300

    Add files via upload

--- src/main/java/com/one/innovation/digital/heroesapi/document/Heroes.java
+++ src/main/java/com/one/innovation/digital/heroesapi/document/Heroes.java
@@ -1,4 +1,45 @@
 package com.one.innovation.digital.heroesapi.document;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+
+@DynamoDBTable(tableName ="Heroes_Table")
+
 public class Heroes {
+
+    /** @DynamoDBHashKey e @DynamoDBAtrribute indicam ao banco qual será o nome das colunas
+     * baseado nos atributos.
+     * O HashKey é só para a chave primeirária
+     */
+    @Id
+    @DynamoDBHashKey(attributeName = "id")
+    private String id;
+
+    @DynamoDBAttribute(attributeName = "name")
+    private String name;
+
+    @DynamoDBAttribute(attributeName = "universe")
+    private String universe;
+
+    @DynamoDBAttribute(attributeName = "films")
+    private int films;
+
+    public Heroes(String id, String name, String universe, int films) {
+        this.id = id;
+        this.name = name;
+        this.universe = universe;
+        this.films = films;
+    }
 }
