c1e59e0c18ae65f323920366c28b33c314aae0d4
vitormanoelcsantos <vitormanoelcsantos@outlook.com>
2021-03-26 16:22:46 -0300

    Add files via upload

---
+++ src/main/java/com/one/innovation/digital/heroesapi/HeroesapiApplication.java
@@ -0,0 +1,12 @@
+package com.one.innovation.digital.heroesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesapiApplication.class, args);
+	}
+}
